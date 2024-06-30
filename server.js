5501394bbaef4cb0bd2bd4c4c63f991f381f7d12
Albert Mwasisoba <almwassy@gmail.com>
2024-06-30 15:24:59 +0300

    feat: validate incoming requests, handle errors
    introduced a function to validate incoming request from arduino and added a global error handler

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+// require('express-async-errors');
 
 const app = express()
-const port = 3001
+
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors())
@@ -17,13 +19,42 @@ let sensorData = {
     gasLevelStatus: ''
 };
 
-app.post('/update', (req, res) => {
+// middleware to verify incoming sensor data
+const validateSensorData = (req, res, next) => {
+    const { temperature, temperatureStatus, humidity, humidityStatus, gasLevel, gasLevelStatus } = req.body;
+
+    if (
+        typeof temperature !== 'number' ||
+        typeof temperatureStatus !== 'string' ||
+        typeof humidity !== 'number' ||
+        typeof humidityStatus !== 'string' ||
+        typeof gasLevel !== 'number' ||
+        typeof gasLevelStatus !== 'string'
+    ) {
+        const error = new Error('Invalid sensor data format');
+        error.status = 400;
+        return next(error);
+    }
+    next();
+}
+
+app.post('/update',validateSensorData, (req, res) => {
     sensorData = req.body;
     res.send('Data received');
 });
 
 app.get('/data', (req, res) => {
     res.json(sensorData);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        error: {
+        message: err.message
+        }
+    });
 });
 
 app.listen(port, () => {
